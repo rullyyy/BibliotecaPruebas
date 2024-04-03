603d365d22479096015b0bea486927206b67b5a3
waw <lcontrerasperaza99@gmail.com>
2024-04-03 01:48:02 -0700

    Agregué una interfaz provisional y una clase DTO para el caso de uso Agregar Usuarios

---
+++ src/main/java/ui/DTOAgregarLibro.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ui;
+
+/**
+ *
+ * @author luisp
+ */
+public class DTOAgregarLibro {
+    private String titulo;
+    private String autor;
+
+    public DTOAgregarLibro() 
+    {
+        
+    }
+
+    public DTOAgregarLibro(String titulo, String autor) {
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+    
+    
+}
